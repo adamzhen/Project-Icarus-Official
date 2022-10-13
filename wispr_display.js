7f6cb26872631b33319703ba9f74d2794999ec78
Adam Zheng <adzcms8@gmail.com>
2022-10-13 11:01:20 -0500

    Fix selector & add slider

--- wispr_display.js
+++ wispr_display.js
@@ -15,6 +15,15 @@ function video(){
     }
 }
 
+var slider = document.getElementById("myRange");
+var output = document.getElementById("slider_value");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
 innerURL = 'https://wispr.nrl.navy.mil/data/rel/pngs/PNGs_Orbit01_inner/'
 innerPNGs = [
     '20181101_004548_1suff.png',
