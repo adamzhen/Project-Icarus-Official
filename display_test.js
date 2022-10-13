2197520dd8358cadcc2ec8b81d87ea43a5480500
Adam Zheng <adzcms8@gmail.com>
2022-10-13 18:46:54 -0500

    Testing readFileSync

--- display_test.js
+++ display_test.js
@@ -1,4 +1,4 @@
-//import { readFileSync, promises as fsPromises } from 'fs';
+import { readFileSync, promises as fsPromises } from 'fs';
 
 var e = document.getElementById("orbit_num");
 var orbit = e.value;
