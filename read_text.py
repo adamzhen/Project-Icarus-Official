86167cd323d41422eea2375e42b589f4d44dcf5c
Adam Zheng <adzcms8@gmail.com>
2022-10-20 16:20:29 -0500

    Converting FITS txt data into a formatted 3D array

--- read_text.py
+++ read_text.py
@@ -7,18 +7,33 @@ def format_orbit(orbit):
     else:
         return 'orbit'+str(o)
 # orbits_list = os.listdir(f'wispr_txt_data')
-
-orbit = format_orbit(11) # set orbit number
-f = open(f'wispr_txt_data/{orbit}.txt','r')
-lines = f.readlines()
 arr = []
-for line in lines:
-    row = []
-    rline = line.replace('\n','').split(',')
-    for j in rline:
-        row.append(j)
-    arr.append(row)
-print(arr)
+with open(f'wispr_list_data/fits_lists.txt', 'w') as l:
+    l.write('[')
+    for n in range(1,12):
+        orbit = format_orbit(n) # set orbit number
+        f = open(f'wispr_txt_data/{orbit}.txt','r')
+        lines = f.readlines()
+        l.write('[')
+        for i in range(len(lines)):
+            line = lines[i]
+            output = "["
+            rline = line.replace('\n','').split(', ')
+            for j in rline:
+                if '.' in j: #converts to float if the string is a float, since only the floats contain '.'
+                    output += f'{j}, '
+                else:
+                    output += f"'{j}', "
+            output = output[:-2]
+            if i == len(lines)-1:
+                output += ']\n'
+            else:
+                output += '],\n'
+            l.write(output)
+        if n==11:
+            l.write(']]')
+        else:
+            l.write('],\n')
 
 
 
