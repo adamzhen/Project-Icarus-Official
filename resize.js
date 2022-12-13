6219550d1a8d5775a97dfd41a4be474860cefa24
Adam Zheng <adzcms8@gmail.com>
2022-12-13 16:05:32 -0600

    Improved responsiveness
    - Adjusted size of rotating circles
    - Added responsiveness for aurora image, other elements

--- resize.js
+++ resize.js
@@ -1,14 +1,22 @@
+// responsiveness upon resize
+
 function resizeDisplay(){
-  if (window.innerWidth <= 1536){
-    document.getElementById("positiondisplay").style.transform = "scale(" + window.innerWidth / 1536 +")";
-  }
+  // The Sun
   var suntexts = document.querySelectorAll('.suntext')
   suntexts.forEach((el) => el.style.transform = "scale(" + window.innerWidth / 1536 +")");
+  // PSP
+  // Corona
+  document.getElementById("auroracontainer").style.setProperty("--w", (window.innerWidth / 1536 * 400) + "px");
+  document.getElementById("auroracontainer").style.width = "scale(" + window.innerWidth / 1536 +")";
+  // WISPR
   document.getElementById("fasterbutton1").style.left = (window.innerWidth/2 + 30) + "px";
   document.getElementById("fasterbutton2").style.left = (window.innerWidth/2 + 40) + "px";
   document.getElementById("slowerbutton1").style.right = (window.innerWidth/2 + 30) + "px";
   document.getElementById("slowerbutton2").style.right = (window.innerWidth/2 + 40) + "px";
+  if (window.innerWidth <= 1536){
+    document.getElementById("positiondisplay").style.transform = "scale(" + window.innerWidth / 1536 +")";
+  }
 }
 
-window.addEventListener("resize", resizeDisplay);
 resizeDisplay();
+window.addEventListener("resize", resizeDisplay);
