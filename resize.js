b3177b530a4508b4da0d83025cd71484425c5ca7
Adam Zheng <adzcms8@gmail.com>
2022-12-19 20:01:06 -0500

    Responsiveness for About page
    - Implemented responsiveness for the about page upon resizing

--- resize.js
+++ resize.js
@@ -3,24 +3,26 @@
 function resizeDisplay(){
   // The Sun
   var suntexts = document.querySelectorAll('.suntext')
-  suntexts.forEach((el) => el.style.transform = "scale(" + window.innerWidth / 1536 +")");
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  suntexts.forEach((el) => el.style.transform = "scale(" + w / 1536 +")");
   // PSP
   // Corona
-  document.getElementById("auroracontainer").style.setProperty("--w", (window.innerWidth / 1536 * 500) + "px");
-  if (window.innerWidth <= 750){
-    document.getElementById("cmecontainer").style.setProperty("--w", (window.innerWidth / 750 * 360) + "px");
-    document.getElementById("carringtoncontainer").style.setProperty("--w", (window.innerWidth / 750 * 330) + "px");
+  document.getElementById("auroracontainer").style.setProperty("--w", (w / 1536 * 500) + "px");
+  if (w <= 750){
+    document.getElementById("cmecontainer").style.setProperty("--w", (w / 750 * 360) + "px");
+    document.getElementById("carringtoncontainer").style.setProperty("--w", (w / 750 * 330) + "px");
   } else {
     document.getElementById("cmecontainer").style.setProperty("--w", "360px");
     document.getElementById("carringtoncontainer").style.setProperty("--w", "330px");
   }
   // WISPR
-  document.getElementById("fasterbutton1").style.left = (window.innerWidth/2 + 30) + "px";
-  document.getElementById("fasterbutton2").style.left = (window.innerWidth/2 + 40) + "px";
-  document.getElementById("slowerbutton1").style.right = (window.innerWidth/2 + 30) + "px";
-  document.getElementById("slowerbutton2").style.right = (window.innerWidth/2 + 40) + "px";
-  if (window.innerWidth <= 1536){
-    document.getElementById("positiondisplay").style.transform = "scale(" + window.innerWidth / 1536 +")";
+  document.getElementById("fasterbutton1").style.left = (w/2 + 30) + "px";
+  document.getElementById("fasterbutton2").style.left = (w/2 + 40) + "px";
+  document.getElementById("slowerbutton1").style.right = (w/2 + 30) + "px";
+  document.getElementById("slowerbutton2").style.right = (w/2 + 40) + "px";
+  if (w <= 1536){
+    document.getElementById("positiondisplay").style.transform = "scale(" + w / 1536 +")";
   }
 }
 
