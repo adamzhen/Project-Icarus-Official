e8fa20f4a167130afa7e7f54b4d26d69f366dd20
Adam Zheng <adzcms8@gmail.com>
2023-02-03 13:19:24 -0600

    Further optimizing
    - Set sweap & fields data lists to defer
    - Reduced size of CoronaSunVIDEO.mp4
    - Changed the automatic days calculation in the about page from pyscript to javascript, reducing load time

--- test.js
+++ test.js
@@ -1,2 +1,5 @@
-var spc = [];
-console.log(spc.indexOf('20181101_0000'));
+var startdate = new Date('10/3/2022');
+var currdate = new Date();
+const diffTime = Math.abs(currdate - startdate);
+const diffDays = Math.round(diffTime / (1000 * 60 * 60 * 24)); 
+console.log(diffDays + " days");
